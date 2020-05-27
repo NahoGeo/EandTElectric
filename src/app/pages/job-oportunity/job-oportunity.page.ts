8292c4637a6ebc04fe27d3eec9e7f4801d5b2401
NahoGeo <oscargbc2012@gmail.com>
2020-05-27 03:48:28 -0600

    Se realizaron algunos cambios de estilo y se trabajó en la página de "Job Opportunity"

--- src/app/pages/job-oportunity/job-oportunity.page.ts
+++ src/app/pages/job-oportunity/job-oportunity.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { set, get } from 'src/app/services/storage';
 
 @Component({
   selector: 'app-job-oportunity',
@@ -7,9 +9,117 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobOportunityPage implements OnInit {
 
-  constructor() { }
+  alldisabled: boolean
+
+  options = [
+    {
+      option: 'Apprentice 1st year',
+      icon: 'business',
+      path: ''
+    },
+    {
+      option: 'Apprentice 2nd year',
+      icon: 'business',
+      path: ''
+    },
+    {
+      option: 'Apprentice 3th year',
+      icon: 'business',
+      path: ''
+    },
+    {
+      option: 'Apprentice 4th year',
+      icon: 'business',
+      path: ''
+    },
+    {
+      option: 'Journeyman',
+      icon: 'business',
+      path: ''
+    },
+    {
+      option: 'Master Electrician',
+      icon: 'business',
+      path: ''
+    },
+    {
+      option: 'Electrician Estimator/Proyect Manager',
+      icon: 'business',
+      path: ''
+    }
+  ]
+
+  constructor(private alertCtrl: AlertController) {
+    this.setInProcess()
+  }
 
   ngOnInit() {
+  }
+  
+  async setInProcess() {
+    const value = await get('inProcess')
+    
+    if (!value) {
+      this.showNote()
+      this.setAllDisableOff()
+    }else {
+      const position = await get('positionSelected')
+      set('inProcess', true)
+      this.alldisabled = true
+      this.applicationInProcess(position)
+    }
+  }
+
+  setAllDisableOn(position: string) {
+    set('inProcess', true)
+    set('positionSelected', `${position}`)
+    this.alldisabled = true
+  }
+
+  setAllDisableOff() {
+    set('inProcess', false)
+    this.alldisabled = false
+  }
+
+  async showNote() {
+    let note = await this.alertCtrl.create({
+      header: 'Note:',
+      message: 'We presente various positions options to you.<br>You can select only one of them.',
+      buttons: [{
+        text: 'ok',
+        role: 'cancel'
+      }]
+    })
+    return note.present()
+  }
+
+  blockSelectOption(position: string) {
+    this.selectionMessage(position)
+    this.setAllDisableOn(position)
+  }
+
+  async selectionMessage(position: string) {
+    let note = await this.alertCtrl.create({
+      header: 'What is next?:',
+      message: `We will send your profile to our HHRR department with the position "${position}" that you selected.`,
+      buttons: [{
+        text: 'ok',
+        role: 'cancel'
+      }]
+    })
+    return note.present()
+  }
+
+  async applicationInProcess(position) {
+    let note = await this.alertCtrl.create({
+      header: 'What is next?:',
+      message: `You already applyed for the position "${position}"<br>Please wait and we call you as soon as posible.`,
+      buttons: [{
+        text: 'ok',
+        role: 'cancel'
+      }]
+    })
+    return note.present()
   }
 
 }
